6b935e6426041ed8f9d867fd955213d6beb0e021
Mohit Shyoran <mohitshyoran2014@gmail.com>
2023-10-15 17:20:05 +0530

    Easy

--- 2. Two Pointers/125. Valid Palindrome.cpp
+++ 2. Two Pointers/125. Valid Palindrome.cpp
@@ -3,10 +3,21 @@ Given a string s, return true if it is a palindrome, or false otherwise.
 
 class Solution {
 public:
+    bool isAphaNumeric(char ch){
+        return (ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
+    }
+    bool isSame(char ch1, char ch2){
+        return (tolower(ch1) == tolower(ch2));
+    }
     bool isPalindrome(string s) {
-        int l = 0, r = s.size();
+
+        int l = 0, r = s.size()-1;
+        
         while(l <= r){
-            if(s[l] != s[r])    return false;
+            while(l < s.size() && !isAphaNumeric(s[l]))    l++;
+            while(r >= 0 && !isAphaNumeric(s[r]))    r--;
+            if(l > r)   return true;
+            if(!isSame(s[l], s[r])) return false;
             l++;
             r--;
         }
