743331331ead1b028cf0fd895584445854df2155
Mohit Shyoran <mohitshyoran2014@gmail.com>
2023-10-21 23:47:02 +0530

    Easy

--- 5. HasMap/205. Isomorphic Strings.cpp
+++ 5. HasMap/205. Isomorphic Strings.cpp
@@ -16,8 +16,11 @@ Input: s = "paper", t = "title"
 Output: true
 
 // Approach
-// take two map and store index of ith character from both string in diffrent maps
-// so either now both character should be coming first time or if came earlier it's corresponding value should be same in both map
+// The idea is that we need to map a char to another one, for example, "egg" and "add", we need to constract the mapping 'e' -> 'a' and 'g' -> 'd'. Instead of directly mapping 'e' to 'a', 
+// another way is to mark them with same value, for example, 'e' -> 1, 'a'-> 1, and 'g' -> 2, 'd' -> 2, this works same.
+//     So we use two arrays here m1 and m2, initialized space is 256 (Since the whole ASCII size is 256, 128 also works here). Traverse the character of both s and t on the same position,
+// if their mapping values in m1 and m2 are different, means they are not mapping correctly, returen false; else we construct the mapping, 
+// since m1 and m2 are both initialized as 0, we want to use a new value when i == 0, so i + 1 works here.
 
 class Solution {
 public:
