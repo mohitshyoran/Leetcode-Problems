46e00020ef0c6aae900e190c5bc8e9a3a9d944b6
Mohit Shyoran <mohitshyoran2014@gmail.com>
2023-10-22 12:26:54 +0530

    Easy

---
+++ 5. HasMap/1. Two Sum.cpp
@@ -0,0 +1,18 @@
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        for(int i = 0; i < nums.size(); i++){
+            int needed = target - nums[i];
+            if(mp.find(needed) != mp.end()){
+                return {i, mp[needed]};
+            }
+            mp[nums[i]] = i;
+        }
+        return {-1,-1};
+    }
+};
