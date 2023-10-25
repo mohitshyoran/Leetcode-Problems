d30dadfc2342683369bd9cf059598bee9821ebdf
Mohit Shyoran <mohitshyoran2014@gmail.com>
2023-10-26 01:24:50 +0530

    Easy

---
+++ 9. Binary Tree General/100. Same Tree.cpp
@@ -0,0 +1,13 @@
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if((p && !q) || (q && !p))  return false;
+        else if(!p && !q) return true;
+        if(p->val != q->val)    return false;
+
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
