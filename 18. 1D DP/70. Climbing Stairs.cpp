e80f1e9770ab5d46ab903134e4e8c0193dc1c58b
Mohit Shyoran <mohitshyoran2014@gmail.com>
2023-11-04 02:54:56 +0530

    Easy.cpp

---
+++ 18. 1D DP/70. Climbing Stairs.cpp
@@ -0,0 +1,27 @@
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 1)  return 1;
+        if(n == 2)  return 2;
+        int a = 1, b = 2, res;
+        for(int i = 3; i <= n; i++){
+            res = a + b;
+            a = b;
+            b = res;
+        }
+        return res;
+    }
+};
+
